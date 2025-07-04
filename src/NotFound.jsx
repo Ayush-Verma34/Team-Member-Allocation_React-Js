9e8651895eefada1467d915745ba104c362397df
Aysuh-Verma34 <ayush2003sitapur@gmail.com>
2025-07-04 09:47:25 +0530

    Final Commit

---
+++ src/NotFound.jsx
@@ -0,0 +1,12 @@
+const NotFound = () => {
+  return (
+    <main className="contaier">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h1 className="text-danger">404-Page Not Found</h1>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default NotFound;
